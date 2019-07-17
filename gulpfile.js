1b5e35cd9f9765d5591c7171e81a5bb24988325c
Brandon Tong <brandon.tong14@gmail.com>
2019-07-17 12:50:29 -0400

    site ready to go

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,35 @@
 // pulls the function from package.json
 
 var gulp = require('gulp')
-var sass = require('gulp-sass')
+var postCss = require('gulp-postcss')
 var cleanCss = require("gulp-clean-css")
 var sourceMaps = require("gulp-sourcemaps")
-
+// broweser refresh
 var browserSync = require('browser-sync').create()
+// images
 var imagemin = require("gulp-imagemin")
+var concat  = require("gulp-concat")
 
-sass.compiler = require('node-sass')
 
-gulp.task("sass", function() {
-  // We want to run sass css/app.scss app.css --watch
-  // this section simply alters the scss file to css file
-  return gulp.src("src/css/app.scss")
+gulp.task("postCss", function() {
+  // We want to run sass css/app.css app.css --watch
+  // this section simply alters the css file to css file
+  return gulp.src([
+    "src/css/reset.css",
+    "src/css/app.css",
+    "src/css/typography.css"
+  ])
     .pipe(sourceMaps.init())
-    .pipe(sass())
+    .pipe(
+      postCss([
+        require("autoprefixer"),
+        require("postcss-preset-env")({
+          stage: 1,
+          browsers: ["IE 11", "last 2 versions"]
+        })
+      ])
+    )
+    .pipe(concat("app.css"))
     .pipe(
       cleanCss({
         compatibility: 'ie8'
@@ -44,7 +58,7 @@ gulp.task("images", function() {
     .pipe(gulp.dest("dist/img"))
 })
 // when you run "gulp watch"  it begins the watch function, whereas
-// if a change is made to scss file, it will run "sass" functions
+// if a change is made to css file, it will run "sass" functions
 // which will update .css file
 gulp.task("watch", function() {
 
@@ -56,7 +70,7 @@ gulp.task("watch", function() {
   })
   // watch for changes
   gulp.watch("src/*.html", ["html"]).on("change", browserSync.reload)
-  gulp.watch("src/css/app.scss", ["sass"])
+  gulp.watch("src/css/*", ["postCss"])
   gulp.watch("src/fonts/*", ["fonts"])
   gulp.watch("src/img/*", ["images"])
 })
@@ -64,4 +78,4 @@ gulp.task("watch", function() {
 
 
 // will run the "sass" function above,  so we can associate many functions with the default
-gulp.task('default', ["sass", "watch", "html", "fonts", "images"])
+gulp.task('default', ["postCss", "watch", "html", "fonts", "images"])
